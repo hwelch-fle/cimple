92b429099b169e87fafe73c904766e5983aa9e93
hwelch-fle <91618355+hwelch-fle@users.noreply.github.com>
2025-12-31 16:40:48 -0500

    re-factor __init__ with submodules

--- src/cimple/__init__.py
+++ src/cimple/__init__.py
@@ -1,34 +1,18 @@
-from pathlib import Path
-from ._build import (
-    build_cim as _build_cim,
-    __version__,
-    __arcpy_version__,
-)
 
-_CIM_BUILT = (Path(__file__).parent / 'cim').exists()
-
-# If this module is imported and the cim submodule isn't generated, 
-# generate it
-if not _CIM_BUILT:
-    print(f'Building cimple.cim')
-    _build_cim()
-
-elif _CIM_BUILT:
-    try:
-        from .cim import (
-            __cim_version__, 
-            __version__ as __existing_version__,
-        )
-        if (
-            __cim_version__ < __arcpy_version__  # type: ignore
-            or 
-            __existing_version__ < __version__
-        ):
-            print(f'Updating cimple.cim')
-            _build_cim()
-    except Exception as e:
-        print(e)
-        print(f'Rebuilding cimple.cim')
-        _build_cim()
+from ._build import check_cimple, __version__, __arcpy_version__
+check_cimple(__file__)
 
 from .cim import *
+from .conversion import (
+    json_to_cim as json_to_cim, 
+    cim_to_json as cim_to_json, 
+    JSONCIMDecoder as JSONCIMDecoder, 
+    JSONCIMEncoder as JSONCIMEncoder,
+    cim_to_cimple as cim_to_cimple,
+    cimple_to_cim as cimple_to_cim,
+)
+
+if __name__ == '__main__':
+    # rebuild the cim when run
+    from ._build import build_cim
+    build_cim()
